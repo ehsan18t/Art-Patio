7ecc9df3252e22f30cb15e64486c0934f5ca6a5e
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-07 19:39:23 +0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
     const DB_HOST = "localhost";
-    const DB_USER = "mithila";
-    const DB_PASS = "Mithila1234";
+    const DB_USER = "root";
+    const DB_PASS = "";
     const DB_NAME = "user_db";
     const SITE_DOMAIN = 'http://localhost';
     const SITE_DIR = '/Art-Patio';
